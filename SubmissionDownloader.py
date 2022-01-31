d1d0e5c6bb3bb0459efe691c50b8e1fdd607ff6a
Benjamin Elbers <elbersb@gmail.com>
2022-01-31 15:54:08 +0100

    configure black code formatter

--- SubmissionDownloader.py
+++ SubmissionDownloader.py
@@ -11,46 +11,50 @@ import tqdm
 
 
 class SubmissionDownloader:
-    PUSHSHIFT_URL = 'https://api.pushshift.io/reddit/search/submission/'
+    PUSHSHIFT_URL = "https://api.pushshift.io/reddit/search/submission/"
 
     def __init__(self, pushshift, reddit):
-        self.pushshift_session = CachedSession(pushshift, backend='sqlite')
+        self.pushshift_session = CachedSession(pushshift, backend="sqlite")
         self.reddit_cache = diskcache.Cache(reddit)
         self.praw_reddit = None
 
-    def _pushshift_api(self, subreddit: str,
-                       start: arrow.arrow.Arrow,
-                       end: arrow.arrow.Arrow) -> Tuple:
+    def _pushshift_api(
+        self, subreddit: str, start: arrow.arrow.Arrow, end: arrow.arrow.Arrow
+    ) -> Tuple:
         payload = {
-            'subreddit': subreddit,
-            'size': 500,
-            'before': round(end.timestamp()),
-            'after': round(start.timestamp())
+            "subreddit": subreddit,
+            "size": 500,
+            "before": round(end.timestamp()),
+            "after": round(start.timestamp()),
         }
         r = self.pushshift_session.get(self.PUSHSHIFT_URL, params=payload)
-        return (r.json()['data'], r.from_cache)
+        return (r.json()["data"], r.from_cache)
 
-    def get_subreddit_submissions(self,
-                                  subreddit: str,
-                                  start: arrow.arrow.Arrow,
-                                  end: arrow.arrow.Arrow,
-                                  hour_window: Optional[int] = 4,
-                                  wait_time: Optional[float] = 0.1) -> List:
-        """ Uses the Pushshift API to obtain the submissions to a subreddit
-            between 'start' and 'end'."""
+    def get_subreddit_submissions(
+        self,
+        subreddit: str,
+        start: arrow.arrow.Arrow,
+        end: arrow.arrow.Arrow,
+        hour_window: Optional[int] = 4,
+        wait_time: Optional[float] = 0.1,
+    ) -> List:
+        """Uses the Pushshift API to obtain the submissions to a subreddit
+        between 'start' and 'end'."""
         all_data = []
-        t = tqdm.tqdm(total=(end-start).days * 8)  # roughly
+        t = tqdm.tqdm(total=(end - start).days * 8)  # roughly
         while start < end:
             t.update(1)
             end_window = start.shift(hours=hour_window)
             data, cached = self._pushshift_api(subreddit, start, end_window)
             all_data += data
             if len(data) >= 100:
-                warnings.warn("Maximum number of items returned. Consider " +
-                              "reducing hour_window.")
+                warnings.warn(
+                    "Maximum number of items returned. Consider "
+                    + "reducing hour_window."
+                )
 
             if len(data):
-                start = arrow.get(data[-1]['created_utc'])
+                start = arrow.get(data[-1]["created_utc"])
             else:
                 start = end_window
 
@@ -69,22 +73,22 @@ class SubmissionDownloader:
             self.reddit_cache[ids] = data
             return data
 
-    def update_subreddit_submissions(self,
-                                     submissions: List,
-                                     attributes: List[str]) -> List:
-        """ Uses the official Reddit API to update submissions
-            with current data."""
+    def update_subreddit_submissions(
+        self, submissions: List, attributes: List[str]
+    ) -> List:
+        """Uses the official Reddit API to update submissions
+        with current data."""
         self.praw_reddit = praw.Reddit()
 
         # process submissions in chunks of 100
-        t = tqdm.tqdm(total=len(submissions)//100+1)
+        t = tqdm.tqdm(total=len(submissions) // 100 + 1)
         for chunk_index in range(0, len(submissions), 100):
             t.update(1)
-            chunk = submissions[chunk_index:chunk_index+100]
+            chunk = submissions[chunk_index : chunk_index + 100]
             reddit_data = self._reddit_api([f"t3_{s['id']}" for s in chunk])
             for index, item in enumerate(reddit_data):
                 for attr in attributes:
-                    submissions[chunk_index+index][attr] = getattr(item, attr)
+                    submissions[chunk_index + index][attr] = getattr(item, attr)
         t.close()
 
         return submissions
